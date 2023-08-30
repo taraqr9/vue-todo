b209b81cedfbb7ebc64b34017703a45b48b70f16
anti3mac <anti3mac@gmail.com>
2023-08-30 08:37:25 +0600

    WIP Edit todo

--- src/App.vue
+++ src/App.vue
@@ -2,26 +2,63 @@
 import { ref, reactive, onMounted } from "vue";
 import axios from "axios";
 
-const baseUrl = "http://localhost:3001/";
+const baseUrl = "http://localhost:3001";
 const todos = reactive({});
-const statuses = ref([]);
+const statuses = reactive({});
+
+const todo = ref({
+  name: "",
+  status: "",
+  created_at: new Date().toLocaleString("en-US", { timeZone: "Asia/Dhaka" }),
+});
 
 async function getTodos() {
-  const res = await fetch(baseUrl + "todos");
+  const res = await fetch(`${baseUrl}/todos`);
   Object.assign(todos, await res.json());
+}
+
+async function create() {
+  await fetch(`${baseUrl}/todos`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(todo.value),
+  });
+
+  alert("Todo created successfully!");
+
+  todo.value.name = "";
+  todo.value.status = "";
+  todo.value.created_at = "";
+}
+
+async function update(todo) {
+  todo.name = "updated Name";
+  todo.status = "updated Status";
+
+  await fetch(`${baseUrl}/todos/${todo.id}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(todo),
+  });
+
+  alert("Todo updated successfully!");
 }
 
 async function getAllStatus() {
   const res = await fetch(`${baseUrl}/statuses`);
-  statuses.value = await res.json();
-  console.log("Statuses:", statuses.value);
-  console.log("helosdffffffffffffffffffffffffffffffffffffffffffffff");
+  Object.assign(statuses, await res.json());
 }
 
 async function destroy(id) {
-  axios.delete(`http://localhost:3001/todos/` + id).then((res) => {
-    getTodos();
-  });
+  let x = window.confirm("You want to delete the todo?");
+
+  if (x) {
+    axios.delete(`${baseUrl}/todos/` + id);
+  }
 }
 
 onMounted(() => {
@@ -33,6 +70,7 @@ onMounted(() => {
 <template>
   <h1 className="text-3xl m-3 font-bold underline">Todo List!</h1>
 
+  <!--------------------------------------------------- Search Bar -->
   <div class="flex">
     <div class="flex-initial w-70 m-1 ...">
       <input
@@ -52,16 +90,37 @@ onMounted(() => {
     </div>
   </div>
 
+  <!------------------------------------------- Create Todo -->
+
+  <div>
+    <h1>Add Todo</h1>
+    <form @submit.prevent="create">
+      <label for="name">Name:</label>
+      <input v-model="todo.name" id="name" required />
+
+      <label for="status">Status:</label>
+      <input v-model="todo.status" id="status" required />
+
+      <button type="submit">Add</button>
+    </form>
+  </div>
+
+  <!------------------------------------------- Status ---------------------------->
   <h2 class="text-xl m-3 font-semibold">Statuses:</h2>
-  //NOTE - Show status output
+
   <ul>
-    <li v-for="status in statuses" :key="status">{{ status }} HY</li>
+    <li v-for="status in statuses" :key="status.id">
+      <input type="checkbox" class="checkbox checkbox-accent" />
+
+      {{ status.status }}
+    </li>
   </ul>
 
+  <!--------------------------------------------- Todos --------------------------->
   <ul role="list" class="border rounded-lg divide-y divide-gray-100">
     <li
       v-for="todo in todos"
-      :key="todo.email"
+      :key="todo.id"
       class="flex m-5 justify-between gap-x-6 py-5"
     >
       {{ todo.id }}
@@ -98,8 +157,14 @@ onMounted(() => {
       <div class="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
         <div class="mt-1 flex items-center gap-x-1.5">
           <button
+            @click="update(todo)"
+            class="flex-none rounded-full bg-yellow-500 p-1"
+          >
+            Edit
+          </button>
+          <button
             @click="destroy(todo.id)"
-            class="flex-none rounded-full bg-red-600/20 p-1"
+            class="flex-none rounded-full bg-red-600 p-1"
           >
             Delete
           </button>
