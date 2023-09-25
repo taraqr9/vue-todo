8f059e43f3a086a66584a854c10ef62039786e6d
taraqjatri <130284178+taraqjatri@users.noreply.github.com>
2023-09-25 11:22:31 +0600

    Fix next page bug

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -308,7 +308,7 @@ onBeforeMount(async () => {
             </li>
 
             <!-- Next Page Button -->
-            <li @click="stateTodo.nextPage"  v-if="filteredTodosLength===10">
+            <li @click="stateTodo.nextPage"  v-if="stateUser.totalTodos>10 && filteredTodosLength===10">
               <a
                   href="#"
                   class="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 rounded-r-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
