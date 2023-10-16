33e5b72e9c4e3335158d9c739966748596232c12
MD.Taraq Rahman <taraqr9@gmail.com>
2023-10-16 12:25:04 +0600

    Update todo create

--- src/main.js
+++ src/main.js
@@ -1,78 +1,17 @@
-import {createApp, reactive, ref} from 'vue'
+import {createApp} from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from './router/index.js'
 import Toaster from "@meforma/vue-toaster";
 import {createPinia} from 'pinia'
-import axios from "axios";
 
 const pinia = createPinia();
 
-(async () => {
-    let localUser = JSON.parse(localStorage.getItem('user'));
-    let localAccessToken = localStorage.getItem('accessToken');
-    let localAuth = JSON.parse(localStorage.getItem('auth'));
+const app = createApp(App)
 
-    const accessToken = ref('');
-    const auth = ref(false);
-    const totalTodos = ref(0);
+app.use(createPinia())
+    .use(router)
+    .use(Toaster)
+    .use(pinia);
 
-    if (localUser && localUser !== "" && localAuth === true) {
-        const dbUser = reactive({});
-        const dbUserAuth = reactive({});
-
-        if (localAccessToken) {
-            localAccessToken = JSON.parse(localAccessToken);
-            localAccessToken = localAccessToken.split('|')
-
-            await axios.get(`http://localhost:3001/tokens?user_id=${localUser.id}&token=${localAccessToken[1]}`).then((res) => {
-                if(res.data.length !== 0){
-                    Object.assign(dbUserAuth, res.data[0]);
-                }else{
-                    localStorage.setItem('user',JSON.stringify(""));
-                    localStorage.setItem('accessToken',JSON.stringify(""));
-                    localStorage.setItem('totalTodos',JSON.stringify(0));
-                    localStorage.setItem('auth',JSON.stringify(false));
-                }
-
-            }).then(async () => {
-                if (localAccessToken[1] === dbUserAuth.token) {
-                    await axios.get(`http://localhost:3001/users?id=${localUser.id}`).then((res) => {
-                        Object.assign(dbUser, res.data[0])
-                    })
-
-                    await axios.get(`http://localhost:3001/todos?user_id=${localAccessToken[0]}`) // Getting total todo
-                        .then(async (res)=>{
-                            let findLength = res.data.filter(function(todo) {
-                                return todo.id;
-                            });
-
-                            totalTodos.value = findLength.length;
-                            accessToken.value = dbUserAuth.user_id + '|' + localAccessToken[1];
-                            auth.value = true;
-                        });
-
-                    delete localUser.password;
-
-
-                    localStorage.setItem('user',JSON.stringify(localUser));
-                    localStorage.setItem('accessToken',JSON.stringify(accessToken.value));
-                    localStorage.setItem('totalTodos',JSON.stringify(totalTodos.value));
-                    localStorage.setItem('auth',JSON.stringify(auth.value));
-                }
-            });
-        }
-
-    }else{
-        localStorage.setItem('user',JSON.stringify(""));
-        localStorage.setItem('accessToken',JSON.stringify(""));
-        localStorage.setItem('totalTodos',JSON.stringify(0));
-        localStorage.setItem('auth',JSON.stringify(false));
-    }
-
-    const app = createApp(App)
-        .use(router)
-        .use(Toaster)
-        .use(pinia);
-    app.mount('#app');
-})();
+app.mount('#app')
--- src/views/Create.vue
+++ src/views/Create.vue
@@ -1,10 +1,14 @@
 <script setup>
-import {ref, onMounted, onBeforeMount} from "vue";
+import {ref, onMounted} from "vue";
 import {useUserStore} from "../js/user.js";
 import {useTodoStore} from "../js/todo.js";
+import {auth, db} from '../firebase/init.js';
+import {collection, addDoc} from 'firebase/firestore';
+import LoadingAnimation from "./LoadingAnimation.vue";
 
 const stateUser = useUserStore();
 const stateTodo = useTodoStore();
+const loading = ref(false);
 
 const todo = ref({
   user_id: "",
@@ -16,41 +20,31 @@ const todo = ref({
 });
 
 async function create() {
-  todo.value.user_id = stateUser.user.id;
+  loading.value = true;
+  todo.value.user_id = auth.currentUser.uid;
   todo.value.created_at = stateTodo.currentDateTime;
   todo.value.updated_at = stateTodo.currentDateTime;
 
-  if (await stateUser.checkUserAndToken() === true) {
-    await fetch(`${stateUser.dbUrl}/todos`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(todo.value),
-    }).then(() => {
-      stateUser.totalTodos += 1;
-      localStorage.setItem('totalTodos', JSON.stringify(stateUser.totalTodos));
-    });
-
+  const colRef = collection(db, 'todos');
+  if (await addDoc(colRef, todo.value)) {
     stateTodo.toast.success("Todo created successfully!");
 
+    todo.value.user_id = "";
     todo.value.name = "";
     todo.value.status = "";
     todo.value.priority = "";
     todo.value.created_at = "";
     todo.value.updated_at = "";
+  } else {
+    stateTodo.toast.error("Something went wrong!");
   }
+  loading.value = false;
 }
-
 
 onMounted(() => {
   stateTodo.getAllStatus();
   stateTodo.getAllPriorities();
 });
-
-onBeforeMount(() => {
-  stateUser.stateUpdate();
-})
 </script>
 
 <template>
@@ -59,6 +53,14 @@ onBeforeMount(() => {
     <div class="md:col-span-2 bg-teal-50 px-3"></div>
 
     <main class="px-16 py-6 md:col-span-10 bg-gray-100 h-full">
+      <div
+          class="absolute inset-0 z-50"
+          v-if="loading"
+      >
+
+        <LoadingAnimation/>
+      </div>
+
       <div class="navbar bg-gray-300 rounded">
         <div class="flex-1 gap-2">
           <RouterLink to="/" class="btn btn-ghost normal-case text-xl bg-white text-black ">Home</RouterLink>
@@ -66,15 +68,13 @@ onBeforeMount(() => {
 
         <div class="dropdown dropdown-end">
           <label tabindex="0" class="btn btn-ghost btn-circle avatar">
-            <div class="w-10 rounded-full">
-              <img src="../assets/avatar.png"/>
-            </div>
+            <img class="w-10 rounded-full" src="../assets/avatar.png"/>
           </label>
-          <ul tabindex="0" class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow rounded-box w-52  bg-white text-black">
+          <ul tabindex="0"
+              class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow rounded-box w-52  bg-white text-black">
             <li class="justify-between">
               <a>
-                <li>{{ stateUser.user.name }}</li>
-                <span class="badge badge-info gap-2">{{ stateUser.totalTodos }}</span>
+                {{ auth.currentUser.displayName }}<span class="badge badge-info gap-2">{{ stateUser.totalTodos }}</span>
               </a>
               <hr>
             </li>
@@ -92,24 +92,24 @@ onBeforeMount(() => {
           <form class="p-3" @submit.prevent="create()">
             <div class="mb-6">
               <input
-                v-model="todo.name"
-                type="text"
-                class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-96 p-2.5"
-                placeholder="Enter your todo name here"
-                required
+                  v-model="todo.name"
+                  type="text"
+                  class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-96 p-2.5"
+                  placeholder="Enter your todo name here"
+                  required
               />
             </div>
             <div class="mb-6">
               <select
-                v-model="todo.status"
-                class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-96 p-2.5"
-                required
+                  v-model="todo.status"
+                  class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-96 p-2.5"
+                  required
               >
                 <option value="" disabled selected>Select Status</option>
                 <option
-                  v-for="status in stateTodo.statuses"
-                  :key="status.id"
-                  :value="status.name"
+                    v-for="status in stateTodo.statuses"
+                    :key="status.id"
+                    :value="status.name"
                 >
                   {{ status.name }}
                 </option>
@@ -117,23 +117,23 @@ onBeforeMount(() => {
             </div>
             <div class="mb-6">
               <select
-                v-model="todo.priority"
-                class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-96 p-2.5"
-                required
+                  v-model="todo.priority"
+                  class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-96 p-2.5"
+                  required
               >
                 <option value="" disabled selected>Select Priority</option>
                 <option
-                  v-for="priority in stateTodo.priorities"
-                  :key="priority.id"
-                  :value="priority.name"
+                    v-for="priority in stateTodo.priorities"
+                    :key="priority.id"
+                    :value="priority.name"
                 >
                   {{ priority.name }}
                 </option>
               </select>
             </div>
             <button
-              type="submit"
-              class="btn btn-success text-black"
+                type="submit"
+                class="btn btn-success text-black"
             >
               Create
             </button>
